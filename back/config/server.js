127675af1a1636e98a92eefaeedcc8d3927832c8
ben <benkrejci@gmail.com>
2021-02-22 18:54:59 +0000

    schema

--- back/config/server.js
+++ back/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 1339),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '76f798895bad1d2d3ad7cc99fa729fd0'),
