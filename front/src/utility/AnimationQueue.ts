1e8184402cc7673bdf6ca3bf1ccf4d62d68eb445
ben <benkrejci@gmail.com>
2021-03-01 18:16:18 -0500

    Exponential delay for AnimationQueue

--- front/src/utility/AnimationQueue.ts
+++ front/src/utility/AnimationQueue.ts
@@ -1,14 +1,18 @@
 import { useRef } from 'react'
 
-const DEFAULT_DELAY_MS = 80
+const DEFAULT_DELAY_MS = 140
 
+/**
+ * Triggers animation events sequentially with some delay in-between starting each.
+ * The more queued animations, the shorter the delay to the next one.
+ */
 export class AnimationQueue {
-  private delayMs: number
+  private targetDelayMs: number
   private queue: Function[]
   private currentTimeout: NodeJS.Timeout | null = null
 
-  constructor(delayMs: number = DEFAULT_DELAY_MS) {
-    this.delayMs = delayMs
+  constructor(targetDelayMs: number = DEFAULT_DELAY_MS) {
+    this.targetDelayMs = targetDelayMs
     this.queue = []
   }
 
@@ -22,8 +26,8 @@ export class AnimationQueue {
     if (index > -1) this.queue.splice(index, 1)
   }
 
-  public setDelay(delayMs: number) {
-    this.delayMs = delayMs
+  public setTargetDelay(delayMs: number) {
+    this.targetDelayMs = delayMs
   }
 
   private next(): void {
@@ -33,13 +37,21 @@ export class AnimationQueue {
     this.currentTimeout = setTimeout(() => {
       this.currentTimeout = null
       this.next()
-    }, this.delayMs)
+    }, this.getDelay())
+  }
+
+  /**
+   * As queue.length → 0, delay → targetDelayMs
+   * As queue.length → ∞, delay → 0
+   */
+  private getDelay(): number {
+    return this.targetDelayMs * Math.pow(0.8, this.queue.length)
   }
 }
 
-export const useAnimationQueue = (delay?: number) => {
+export const useAnimationQueue = (targetDelayMs?: number) => {
   const ref = useRef<AnimationQueue>()
   if (!ref.current) ref.current = new AnimationQueue()
-  if (delay !== undefined) ref.current.setDelay(delay)
+  if (targetDelayMs !== undefined) ref.current.setTargetDelay(targetDelayMs)
   return ref.current
 }
