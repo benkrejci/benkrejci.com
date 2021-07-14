2210191036711deff8ff5bd9df4c5c1dc5465b21
⚡Ben Krejci ⚡ <krejci@duolingo.com>
2021-07-14 10:20:53 -0400

    Correct semantic tag for markdown headings

--- front/src/components/widgets/RichText.tsx
+++ front/src/components/widgets/RichText.tsx
@@ -21,17 +21,23 @@ export function RichText({
 
 const styles = (theme) => ({
   listItem: {
-    //marginTop: theme.spacing(1),
+    marginTop: theme.spacing(1),
   },
 })
 
 const options = {
   overrides: {
-    h1: { component: Typography, props: { variant: 'h3', gutterBottom: true } },
-    h2: { component: Typography, props: { variant: 'h4', gutterBottom: true } },
-    h3: { component: Typography, props: { variant: 'h5' } },
-    h4: { component: Typography, props: { variant: 'subtitle1', paragraph: true } },
-    h5: { component: Typography, props: { variant: 'subtitle2', paragraph: true } },
+    h1: { component: Typography, props: { component: 'h1', variant: 'h3', gutterBottom: true } },
+    h2: { component: Typography, props: { component: 'h2', variant: 'h4', gutterBottom: true } },
+    h3: { component: Typography, props: { component: 'h3', variant: 'h5' } },
+    h4: {
+      component: Typography,
+      props: { component: 'h4', variant: 'subtitle1', paragraph: true },
+    },
+    h5: {
+      component: Typography,
+      props: { component: 'h5', variant: 'subtitle2', paragraph: true },
+    },
     p: { component: Typography, props: { paragraph: true } },
     a: { component: Link, props: { target: '_blank' } },
     img: {
