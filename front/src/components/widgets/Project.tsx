777a9cff81db5a18681892844606ca37b7dc3397
ben <benkrejci@gmail.com>
2023-02-26 16:36:21 -0500

    Make project covers big

--- front/src/components/widgets/Project.tsx
+++ front/src/components/widgets/Project.tsx
@@ -28,29 +28,6 @@ export const Project = ({ project }: { project: ProjectModel }): ReactElement =>
     <Box mb={4}>
       <Paper elevation={1}>
         <Box p={4} className={styles.container}>
-          {project.cover ? (
-            <div className={styles.cover}>
-              <Paper elevation={1}>
-                <WrapIf
-                  if={project.url && project.cover?.mime?.startsWith('image/')}
-                  wrapper={
-                    <Link href={project.url} target="_blank" component={ExternalLink}>
-                      {' '}
-                    </Link>
-                  }
-                >
-                  <Media
-                    file={project.cover}
-                    maxHeight="60vh"
-                    style={{ backgroundColor: 'transparent' }}
-                  />
-                </WrapIf>
-              </Paper>
-            </div>
-          ) : (
-            ''
-          )}
-
           <div className={styles.title}>
             <Typography variant="h3" gutterBottom>
               {project.name} <FragmentLink text={project.name} type="link" />
@@ -59,6 +36,29 @@ export const Project = ({ project }: { project: ProjectModel }): ReactElement =>
             <Typography variant="subtitle2">
               Published {project.created_at.toLocaleDateString()}
             </Typography>
+
+            {project.cover ? (
+              <div className={styles.cover}>
+                <Paper elevation={1}>
+                  <WrapIf
+                    if={project.url && project.cover?.mime?.startsWith('image/')}
+                    wrapper={
+                      <Link href={project.url} target="_blank" component={ExternalLink}>
+                        {' '}
+                      </Link>
+                    }
+                  >
+                    <Media
+                      file={project.cover}
+                      maxHeight="60vh"
+                      style={{ backgroundColor: 'transparent' }}
+                    />
+                  </WrapIf>
+                </Paper>
+              </div>
+            ) : (
+              ''
+            )}
 
             <List className={styles.list}>
               {project.links?.map((link) => (
@@ -99,7 +99,17 @@ const useStyles = makeStyles((theme) => ({
     width: 'auto',
     '&, .MuiSvgIcon-root': {
       color: theme.palette.primary.main,
-    }
+    },
+  },
+  title: {
+    width: '100%',
+  },
+  cover: {
+    width: '100%',
+    marginBottom: theme.spacing(2),
+  },
+  description: {
+    width: '100%',
   },
 
   [theme.breakpoints.down('xs')]: {
@@ -107,30 +117,11 @@ const useStyles = makeStyles((theme) => ({
       display: 'flex',
       flexWrap: 'wrap',
     },
-    title: {
-      order: 0,
-      width: '100%',
-    },
-    cover: {
-      order: 1,
-      width: '100%',
-      marginBottom: theme.spacing(2),
-    },
-    description: {
-      order: 2,
-      width: '100%',
-    },
   },
 
   [theme.breakpoints.up('sm')]: {
     title: {
       overflowWrap: 'anywhere',
-    },
-    cover: {
-      float: 'right',
-      width: '500px',
-      maxWidth: '50%',
-      marginLeft: theme.spacing(2),
     },
   },
 }))
